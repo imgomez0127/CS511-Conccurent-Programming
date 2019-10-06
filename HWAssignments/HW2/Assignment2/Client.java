cd15e8d976400dd0bee0cf9c77577abd50008af9
garyung171 <garyung17@gmail.com>
2019-10-06 18:06:32 -0400

    Client.java without print statements implemented

--- HWAssignments/HW2/Assignment2/Client.java
+++ HWAssignments/HW2/Assignment2/Client.java
@@ -1,16 +1,45 @@
 package Assignment2;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
+import java.util.concurrent.Semaphore;
 import Assignment2.Exercise;
+import Assignment2.WeightPlateSize;
+import Assignment2.ApparatusType;
+
 public class Client{
-    public int id;
+    private int id;
     private ArrayList<Exercise> routine;
+
     Client(int id){
         this.id = id;
     }
-    public void addExcercise(Exercise e){
+
+    public void addExercise(Exercise e){
+        this.routine.add(e);
     }
+
     public static Client generateRandom(int id){
-        return null; 
-    } 
+        Client newClient = new Client(id);
+        newClient.generateRoutine();
+        return newClient; 
+    }
+
+    public void generateRoutine() {
+        for (int i = 0; i < 15; i++) {
+            this.routine.addExercise(Exercise.generateRandom());
+        }
+    }
+
+    public void executeRoutine(
+          HashMap<ApparatusType,Semaphore> availableApparatuses,                                          
+          HashMap<WeightPlateSize,Semaphore> availableWeights, 
+          Semaphore tryToGrabWeights 
+    ) {
+        for (Exercise i : routine) {
+            i.performExercise(availableApparatuses, availableWeights, tryToGrabWeights);
+        }
+        // This function executes all the exercises in the routine
+    }
 }
