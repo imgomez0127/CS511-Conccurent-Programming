c17b76990d799050aadaf4b99bdf22a11f0f388f
imgomez0127 <ianm0127@hotmail.com>
2019-10-06 16:08:11 -0400

    Added function to execute exercise

--- HWAssignments/HW2/Assignment2/Exercise.java
+++ HWAssignments/HW2/Assignment2/Exercise.java
@@ -2,6 +2,7 @@ package Assignment2;
 
 import java.util.HashMap;
 import java.util.Random;
+import java.util.concurrent.Semaphore;
 import Assignment2.WeightPlateSize;
 import Assignment2.ApparatusType;
 
@@ -9,19 +10,75 @@ public class Exercise{
     private ApparatusType at;
     private HashMap<WeightPlateSize,Integer> weight;
     private int duration;
-    
+
     Exercise(ApparatusType at, HashMap<WeightPlateSize,Integer> weight, int duration){
         this.at = at;
         this.weight = weight;
         this.duration = duration;
     }
 
-    private ApparatusType pickRandomApparatus(){
+    private static ApparatusType pickRandomApparatus(){
         int apparatusNumber = new Random().nextInt(ApparatusType.values().length);
         return ApparatusType.values()[apparatusNumber];
     }
 
+    private static HashMap<WeightPlateSize,Integer> pickRandomWeights(){
+        HashMap<WeightPlateSize,Integer> excerciseWeights = new HashMap<WeightPlateSize,Integer>();
+        WeightPlateSize[] weightPlates = WeightPlateSize.values();
+        Random randomNumberGenerator = new Random();
+        for(int i = 0; i < weightPlates.length; ++i){
+            int amountOfWeights = randomNumberGenerator.nextInt(10);
+            excerciseWeights.put(weightPlates[i],amountOfWeights);
+        }
+        return excerciseWeights;
+    } 
+    private static int pickRandomDuration(){
+        int randomDuration = new Random().nextInt(9)+1;
+        return randomDuration;
+    }
     public static Exercise generateRandom(){
-        return null;
+        ApparatusType randomApparatus = pickRandomApparatus();
+        HashMap<WeightPlateSize,Integer> weights = pickRandomWeights(); 
+        int duration = pickRandomDuration();
+        return new Exercise(randomApparatus,weights,duration);
+    }
+    private void grabWeights(
+        HashMap<WeightPlateSize,Semaphore> availableWeights
+    ){
+        for(WeightPlateSize weightSize : WeightPlateSize.values()){
+            int amountOfWeightsToGrab = weight.get(weightSize);
+            for(int amountOfGrabbedWeights = 0; 
+                amountOfGrabbedWeights < amountOfWeightsToGrab; 
+                amountOfGrabbedWeights++
+            ){
+                try{
+                    availableWeights.get(weightSize).acquire();
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void performExercise(
+        HashMap<ApparatusType,Semaphore> availableApparatuses, 
+        HashMap<WeightPlateSize,Semaphore> availableWeights, 
+        Semaphore tryToGrabWeights
+    ){
+        try{
+            tryToGrabWeights.acquire();
+            this.grabWeights(availableWeights);
+        }catch(InterruptedException e){
+            e.printStackTrace();
+        }
+        finally{
+            tryToGrabWeights.release(); 
+        }
+        try{
+            availableApparatuses.get(at).acquire();
+            Thread.sleep(duration);
+        }catch(InterruptedException e){
+            e.printStackTrace();
+        }
     }
 }
--- HWAssignments/HW2/Assignment2/Gym.java
+++ HWAssignments/HW2/Assignment2/Gym.java
@@ -2,14 +2,42 @@ package Assignment2;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.concurrent.Semaphore;
 import Assignment2.WeightPlateSize;
-
+import Assignment2.ApparatusType;
 public class Gym implements Runnable{
     public static final int GYM_SIZE = 30;
     public static final int GYM_REGISTERED_CLIENTS = 10000;
     private HashMap<WeightPlateSize,Integer> noOfWeightPlates;
     private HashSet<Integer> clients;
+    private HashMap<WeightPlateSize,Semaphore> availableWeights;
+    private HashMap<ApparatusType,Semaphore> availableApparatuses;
+    private Semaphore grabWeights = new Semaphore(1);
     
+    private void populateAvailableWeightsMap(){
+        this.availableWeights = new HashMap<WeightPlateSize,Semaphore>();
+        int weightSizeAmount[] = {110,90,75};
+        int current_weight = 0;
+        for(WeightPlateSize weightSize : WeightPlateSize.values()){
+            this.availableWeights.put(
+                weightSize,
+                new Semaphore(weightSizeAmount[current_weight])
+            );
+            current_weight++;
+        }
+    }
+    private void populateAvailableApparatusesMap(){
+        this.availableApparatuses = new HashMap<ApparatusType,Semaphore>();
+        for(ApparatusType apparatus:ApparatusType.values()){
+            this.availableApparatuses.put(apparatus,new Semaphore(5));
+        }
+    }
+        
+    Gym(){
+        populateAvailableWeightsMap();
+        populateAvailableApparatusesMap();    
+    }     
+
     public void run(){
     }
 }
