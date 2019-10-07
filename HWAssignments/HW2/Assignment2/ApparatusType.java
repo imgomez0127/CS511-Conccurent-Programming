c7f28d9a56dca8d654c2f96225813ffacf446cb9
imgomez0127 <ianm0127@hotmail.com>
2019-10-06 21:28:31 -0400

    Added pledge

--- HWAssignments/HW2/Assignment2/ApparatusType.java
+++ HWAssignments/HW2/Assignment2/ApparatusType.java
@@ -1,3 +1,9 @@
+/**
+* Assignment 2: Modeling a gym using Semaphores
+* "I pledge my honor that I have abided by the Stevens honor system" - igomez1 Ian Gomez
+* Partner: Gary Ung
+*/
+
 package Assignment2;
 
 public enum ApparatusType {
